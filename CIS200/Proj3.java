4c1c03b1ed5cb64866096451d5f2f320b308c8a0
Jackson Seim <jseim4699@gmail.com>
2023-02-08 19:38:25 -0600

    finished Proj3.java

--- CIS200/Proj3.java
+++ CIS200/Proj3.java
@@ -19,7 +19,7 @@ public class Proj3 {
         char input;
         boolean p1Win = false; boolean p2Win = false;
 
-        /*Part 1
+        //Part 1
         while (p1Total < WIN && p2Total < WIN){
             for (int i = 1; i < 3; i++){
 
@@ -53,14 +53,14 @@ public class Proj3 {
                 else if (p2Total >= WIN){p2Win = true;break;}
             }
         }
-        if (p1Win){System.out.println("Player 1 wins");}
-        else if (p2Win){System.out.println("Player 2 wins");};*/
+        if (p1Win){System.out.println("Player 1 wins\n");}
+        else if (p2Win){System.out.println("Player 2 wins\n");}
 
-
+        System.out.println("Press enter to continue ot Part 2..."); String temp = scan.nextLine();
 
         //Part 2
         p1Total = 0; p1Win = false; p1Turn = 0; dice = -1;
-        int aiTotal = 0; boolean aiWin = false; int aiTurn = 0; boolean aiRoll; int aiRollCount;
+        int aiTotal = 0; boolean aiWin = false; int aiTurn = 0; boolean aiRoll;
 
         while (p1Total < WIN && aiTotal < WIN){
             for (int i = 1; i < 3; i++){
@@ -80,24 +80,29 @@ public class Proj3 {
                     }
                     else {
                         //"algorithm" 
-                        aiRoll = true; aiRollCount = 0; aiTurn = 0;
-                            for (int j = 0; j < 2; j++){
-                                if (aiTurn < WIN){
-                                    System.out.printf("\nComputer turn total is %d. Computer rolls.\n",aiTurn);
-                                    System.out.printf("Computer rolled: %d\n", dice = rand.nextInt(1, 7));
-                                    if (dice == 1){aiTurn = 0; aiRoll = false; break;}
-                                    else{aiTurn+=dice;}
-                                }
-                                else{System.out.printf("\nComputer turn total is %d. Computer stops.\n",aiTurn); aiRoll = false; dice = 1;}
+                        aiRoll = true; aiTurn = 0;
+                        for (int j = 0; j < 2; j++){
+                            if (aiTurn < WIN){
+                                System.out.printf("\nComputer turn total is %d. Computer rolls.\n",aiTurn);
+                                System.out.printf("Computer rolled: %d\n", dice = rand.nextInt(1, 7));
+                                if (dice == 1){aiTurn = 0; aiRoll = false; break;}
+                                else{aiTurn+=dice;}
                             }
-                            if ((aiTurn<10) && p1Total >= 13 && aiTotal < 10){
-                                while (aiTurn < 10) {
-                                    System.out.printf("\nComputer turn total is %d. Computer rolls.\n",aiTurn);
-                                    System.out.printf("Computer rolled: %d\n", dice = rand.nextInt(1, 7));
-                                    if (dice == 1){aiRoll = false; break;}
-                                    else{aiTurn+=dice;}
-                                }
+                            else{System.out.printf("\nComputer turn total is %d. Computer stops.\n",aiTurn); aiRoll = false; dice = 1;}
+                        }
+                        if ((aiTurn<10) && p1Total >= 13 && aiTotal < 10 && aiRoll){
+                            while (aiTurn < 10) {
+                                System.out.printf("\nComputer turn total is %d. Computer rolls 2.\n",aiTurn);
+                                System.out.printf("Computer rolled: %d\n", dice = rand.nextInt(1, 7));
+                                if (dice == 1){aiRoll = false; break;}
+                                else{aiTurn+=dice;}
                             }
+                            System.out.println(dice);
+                            if (dice == 1){aiTurn = 0;}
+                        }
+                        else {System.out.printf("\nComputer turn total is %d. Computer stops\n", aiTurn);}
+                        dice = 1;
+                        aiTotal+=aiTurn;
                     }   
 
 
@@ -108,9 +113,8 @@ public class Proj3 {
                 else if (aiTotal >= WIN){aiWin = true;break;}
             }
         }
-        if (p1Win){System.out.println("\n\nYou win!");}
-        else if (aiWin){System.out.println("Computer wins!");}
-        //computer sets aiTurn to 0 after 2 rolls
+        if (p1Win){System.out.println("\nYou win!\n");}
+        else if (aiWin){System.out.println("\nComputer wins!\n");}
         
     
     
